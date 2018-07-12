9c9dee4fc77bee6554ecccd90a30266c0842a997
HINDREDARTURO <HINDREDARTURO@nino>
2018-07-11 21:40:08 -0400

    Clase contrato con variables de fecha

--- ProyectoFinal/src/logical/Contrato.java
+++ ProyectoFinal/src/logical/Contrato.java
@@ -1,19 +1,26 @@
 package logical;
 
+import java.util.Date;
+
 public class Contrato {
 	private String numCont;
 	private String tipoPago;
 	private Cliente miCliente;
 	private Proyecto miProyecto;
+	private Date fechaIni;
+	private Date fechaFin;
 	
-	public Contrato(String numCont, String tipoPago, Cliente miCliente, Proyecto miProyecto) {
+	public Contrato(String numCont, String tipoPago, Cliente miCliente, Proyecto miProyecto, Date fechaIni,
+			Date fechaFin) {
 		super();
 		this.numCont = numCont;
 		this.tipoPago = tipoPago;
 		this.miCliente = miCliente;
 		this.miProyecto = miProyecto;
+		this.setFechaIni(fechaIni);
+		this.setFechaFin(fechaFin);
 	}
-
+	
 	public String getNumCont() {
 		return numCont;
 	}
@@ -45,6 +52,21 @@ public class Contrato {
 	public void setMiProyecto(Proyecto miProyecto) {
 		this.miProyecto = miProyecto;
 	}
-	
-	
+
+	public Date getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(Date fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public Date getFechaIni() {
+		return fechaIni;
+	}
+
+	public void setFechaIni(Date fechaIni) {
+		this.fechaIni = fechaIni;
+	}
+
 }
