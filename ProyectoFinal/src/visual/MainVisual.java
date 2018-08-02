27490c5b836adb810a46b7bdf7c1b896a662fe45
lisar <lisar@LAPTOP-LA9M2Q38>
2018-08-02 00:37:36 -0400

    primera grafica, librerias graficas

--- ProyectoFinal/src/visual/MainVisual.java
+++ ProyectoFinal/src/visual/MainVisual.java
@@ -7,13 +7,25 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
 import logical.Cliente;
 
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
+import java.awt.CardLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+
 
 public class MainVisual extends JFrame {
 	private JPanel contentPane;
@@ -23,6 +35,36 @@ public class MainVisual extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		 DefaultPieDataset data = new DefaultPieDataset();
+	        data.setValue("C#", 40);
+	        data.setValue("Java", 45);
+	        data.setValue("Python", 15);
+	        data.setValue("C/C++",10);
+	 
+	        // Creando el Grafico
+	        JFreeChart chart = ChartFactory.createPieChart(
+	         "Cantidad de Lenguajes usado por los trabajadores: ", 
+	         data, 
+	         true, 
+	         true, 
+	         false);
+	 
+	        // Mostrar Grafico
+	        
+	        ChartFrame frame = new ChartFrame("JFreeChart", chart);
+	        
+	        try {
+				ChartUtilities.saveChartAsJPEG(new File("grafico.jpg"), chart, 250, 250);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	     
+	        frame.pack();
+	        frame.setVisible(true);
+		
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -138,8 +180,9 @@ public class MainVisual extends JFrame {
 		mnContratos.add(mntmListadoDeContratos);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
+	
 
 }
